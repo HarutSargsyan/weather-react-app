3c1b59de184ed9208862287704f0a14f5fe6bf4a
Harut Sargsyan <sargsyanharut2000@gmail.com>
2022-02-03 23:46:39 +0400

    improvements for footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,17 @@ import Footer from "./components/Footer";
 import usePostCity from "./hooks/usePostCity";
 import useGetDefaultWeather from "./hooks/useDefaultWeather";
 
+const PageWrapper = styled.div`
+  position: relative;
+  min-height: 100vh;
+`;
+
 const ViewWrapper = styled.main`
   padding: 20px 40px;
   background: linear-gradient(to right, #56ab2f, #a8e063);
   clip-path: polygon(0 0, 100% 0, 100% 80%, 0 100%);
   height: 90vh;
+  padding-bottom: 2.5rem;
 `;
 
 export default () => {
@@ -20,7 +26,7 @@ export default () => {
   });
   const { defaultImage, defaultResponse } = useGetDefaultWeather();
   return (
-    <>
+    <PageWrapper>
       <ViewWrapper>
         <SearchBar isError={isError} onTermSubmit={onCitySubmit} />
         <Result
@@ -30,6 +36,6 @@ export default () => {
         />
       </ViewWrapper>
       <Footer />
-    </>
+    </PageWrapper>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => (
-  <footer>
+  <footer
+    style={{
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+      height: " 2.5rem ",
+    }}
+  >
     <h4 style={{ textAlign: "center" }}> &copy; Harut Sargsyan | 2020</h4>
   </footer>
 );
